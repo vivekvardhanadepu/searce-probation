b24c43b2bba5a4e01fabbbd09350defc436a52c1
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-07-29 19:40:54 +0530

    JS programming Primer complete

---
+++ JS/test.js
@@ -0,0 +1,66 @@
+3 + 4;
+43 - 1;
+
+!false;
+
+3.0 == 3;
+
+3.0 === 3;
+4 < 3;
+
+true || false;
+typeof 3;
+typeof 3.0;
+typeof 'hello';
+
+typeof undefined;
+
+typeof null;
+
+typeof [1, 2, 3];
+
+typeof { age: 39 };
+
+typeof 'hello';
+
+var h = 'kyle';
+
+console.log(h);
+
+var age;
+age = 3;
+
+var friends = ['buddu', 'go', 'all ways'];
+
+console.log(friends.length);
+friends.length;
+
+friends[1];
+
+age++;
+
+age;
+
+age += 2;
+
+age = age + age * 2;
+
+if (!age > 10) {
+  console.log('greater');
+} else {
+  console.log('lesser');
+}
+
+/*for(let i=0;i<friends.length;i++){
+  console.log(`Hii ${friends[i]}`);
+}
+
+for(let friend of friends){
+  console.log(`Hii ${friend}`);
+}
+
+while(friends.length >0){
+  let friend = friends.pop();
+  console.log(`Hii ${friend}`);
+}*/
+friends;
