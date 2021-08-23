0fca8709c2c5093c3c6553000c6c2c5409b26991
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-08-23 19:11:55 +0530

    testing with the template data

--- sheets-api/index.js
+++ sheets-api/index.js
@@ -18,25 +18,28 @@ client.authorize((err, tokens) => {
 async function gsrun(cl) {
   const gsapi = google.sheets({ version: "v4", auth: cl });
   const opt = {
-    spreadsheetId: "1KMyjzM4jPh9H0_vLuSDtxMoXfUsetoM1xyUQRwacu5Y",
-    range: "Books!A1:B3",
+    spreadsheetId: "1lOSs2v92YJ6VVsAsK1MwhHRHOsBOtO8C2RoKS5Zi0xY",
+    range: "Sheet1!A1:B3",
   };
   let data = await gsapi.spreadsheets.values.get(opt);
   let dataArray = data.data.values;
+  console.log(data.data.values);
   dataArray = dataArray.map((r) => {
     while (r.length < 2) {
       r.push("");
     }
     return r;
   });
-  let newData = dataArray.map((row) => {
-    row.push(row[0] + "-" + row[1]);
-    return row;
-  });
-  //   console.log(newData);
-  opt.range = "Books!E2";
-  opt.valueInputOption = "USER_ENTERED";
-  opt.resource = { values: newData };
-  let res = await gsapi.spreadsheets.values.update(opt);
-  console.log(res);
+  console.log("after:", dataArray);
+  // let newData = dataArray.map((row) => {
+  //   row.push(row[0] + "-" + row[1]);
+  //   return row;
+  // });
+  // console.log(newData);
+  // //   console.log(newData);
+  // opt.range = "Books!E4";
+  // opt.valueInputOption = "USER_ENTERED";
+  // opt.resource = { values: newData };
+  // let res = await gsapi.spreadsheets.values.update(opt);
+  // console.log(res);
 }
